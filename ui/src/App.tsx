fcdfbcf69146a86fe91e8ed665a10ab08dd7c5ec
Aditya Prakash <adityaprakash6688@gmail.com>
2024-10-06 04:16:51 +0530

    tailwind added

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,12 +1,13 @@
-import { getEnv } from './utils/runtimeConfig';
+import { getEnv } from "./utils/runtimeConfig";
 
 function App() {
-  const apiUrl = getEnv('API_URL');
-  const featureFlag = getEnv('FEATURE_FLAG');
-  const sample = getEnv('SAMPLE');
+  const apiUrl = getEnv("API_URL");
+  const featureFlag = getEnv("FEATURE_FLAG");
+  const sample = getEnv("SAMPLE");
 
   return (
     <div>
+      <h1 className="text-3xl font-bold underline bg-red-600">Hello world!</h1>
       <h1>Runtime Environment Variables</h1>
       <p>API URL: {apiUrl || "NA"}</p>
       <p>Feature Flag: {featureFlag || "NA"}</p>
@@ -15,4 +16,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
